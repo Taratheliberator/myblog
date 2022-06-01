16434ca92445a7933ec5bb3a355224c5cb8b152c
Taratheliberator <Taratheliberator@gmail.com>
2022-06-01 22:22:29 +0500

    init5

--- src/main/java/com/viner/myblog/controllers/BlogController.java
+++ src/main/java/com/viner/myblog/controllers/BlogController.java
@@ -38,6 +38,8 @@ public class BlogController {
     }
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if(!postRepository.existsById(id)){
+        return "redirect:/blog";}
        Optional<Post> post= postRepository.findById(id);
         ArrayList<Post> res =  new ArrayList<>();
         post.ifPresent(res::add);
